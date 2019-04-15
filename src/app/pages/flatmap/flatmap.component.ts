435b1d0a2edc75c40d3dfeb33969bcb571220fa5
Fagner Lira <fagnerlira@hotmail.com>
2019-04-15 20:34:37 -0300

    Nada

--- src/app/pages/flatmap/flatmap.component.ts
+++ src/app/pages/flatmap/flatmap.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { FlatMapModule } from './flatmap.module';
 
 @Component({
   selector: 'app-flatmap',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlatmapComponent implements OnInit {
 
+  produto = new Subject<string[]>();
+
+   a = new Set([1,2,3]);
+     b = new Set([4,3,2]);
+  
   constructor() { }
 
   ngOnInit() {
+
+
+
+      this.produto.next();
 
 
       const cestas = [
@@ -19,6 +30,25 @@ export class FlatmapComponent implements OnInit {
 
      cestas.map(x => x.itens) // [Array(2), Array(2)]
     // cestas.flatMap(x => x.itens) // [“Maça”, “Banana”, “Banana”, “Abacaxi”]
+
+
+    var obj1 = { a: 1 };
+    var obj2 = { b: 2 };
+    var obj3 = { b: 1 };
+    
+    var merged = Object.assign(obj1, obj2, obj3);
+    
+    //console.log(merged); // { a: 1, b: 2 }
+
+
+
+    
+    let  set1 = new Set(['a', 'b']);
+    const set2 = new Set(['b', 'c']);
+    //const intersection = new Set([...set1].filter(x => set2.has(x)));
+
+
+    
 
   }
 
